52db689d3c466e7ae30a57447d33f1ad8559d740
Til Blechschmidt <til@blechschmidt.de>
2015-08-30 16:41:46 +0200

    Added pseudocode

--- CUDA/server.py
+++ CUDA/server.py
@@ -9,3 +9,11 @@ def execute(d_boards, d_actions, d_links):
     if pos < len(d_boards) and pos < len(d_actions) and pos < len(d_links):
         d_board = d_boards[pos]
         d_action = d_actions[pos]
+
+        # Step 1: Look if the action is valid
+
+        # Step 1.1: Is there already a pin?
+
+        # Step 1.2: DO NOT CROSS THE BEAMS ahem LINES
+
+        # Step 2: Check if somebody has won *yay*
