d9540c9a031e85f33325a6e49bd6071650c89a5a
Til Blechschmidt <til@blechschmidt.de>
2015-11-10 18:27:35 +0100

    Copied prototype set_link code in

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 __author__ = ['Til Blechschmidt', 'Noah Peeters', 'Merlin Brandt']
 
 from numba import jit
-
-from config import BOARD_SIZE, BOARD_WIDTH
+from config import BOARD_SIZE, BOARD_WIDTH, board_coord_to_index
 
 
 # from boardlocation import BoardLocation
@@ -22,6 +21,58 @@ def value_in_array(value, array):
 @jit
 def move_is_valid(board, move):
     return BOARD_WIDTH <= move <= BOARD_SIZE - BOARD_WIDTH and board[move] == 0
+
+
+@jit
+def set_link(direction, location, board, links):
+    links[location][direction] = 10  # 10 = Here's a link
+    x = 0
+    y = 0
+    bcti = board_coord_to_index
+    if direction == 0:
+        links[bcti(x, y + 1)][1] += 1  # links[x    ][y + 1].bridge[1] += 1
+        links[bcti(x - 1, y + 1)][2] += 1  # links[x - 1][y + 1].bridge[2] += 1
+        links[bcti(x - 2, y + 1)][2] += 1  # links[x - 2][y + 1].bridge[2] += 1
+        links[bcti(x - 1, y)][3] += 1  # links[x - 1][y    ].bridge[3] += 1
+        links[bcti(x - 1, y)][2] += 1  # links[x - 1][y    ].bridge[2] += 1
+        links[bcti(x - 1, y)][1] += 1  # links[x - 1][y    ].bridge[1] += 1
+        links[bcti(x - 2, y)][3] += 1  # links[x - 2][y    ].bridge[3] += 1
+        links[bcti(x - 2, y)][2] += 1  # links[x - 2][y    ].bridge[2] += 1
+        links[bcti(x - 3, y)][3] += 1  # links[x - 3][y    ].bridge[3] += 1
+
+    if direction == 1:
+        links[x - 1][y + 1].bridge[2] += 1
+        links[x + 1][y].bridge[0] += 1
+        links[x - 1][y].bridge[3] += 1
+        links[x - 1][y].bridge[2] += 1
+        links[x - 2][y].bridge[3] += 1
+        links[x][y - 1].bridge[0] += 1
+        links[x - 1][y - 1].bridge[3] += 1
+        links[x - 1][y - 1].bridge[2] += 1
+        links[x - 2][y - 1].bridge[3] += 1
+
+    if direction == 2:
+        links[x + 1][y + 1].bridge[1] += 1
+        links[x - 1][y].bridge[3] += 1
+        links[x + 1][y].bridge[0] += 1
+        links[x + 1][y].bridge[1] += 1
+        links[x + 2][y].bridge[0] += 1
+        links[x][y - 1].bridge[3] += 1
+        links[x + 1][y - 1].bridge[0] += 1
+        links[x + 1][y - 1].bridge[1] += 1
+        links[x + 2][y - 1].bridge[0] += 1
+
+    if direction == 3:
+        links[x][y + 1].bridge[2] += 1
+        links[x + 1][y + 1].bridge[1] += 1
+        links[x + 2][y + 1].bridge[1] += 1
+        links[x + 1][y].bridge[0] += 1
+        links[x + 1][y].bridge[1] += 1
+        links[x + 1][y].bridge[2] += 1
+        links[x + 2][y].bridge[0] += 1
+        links[x + 2][y].bridge[1] += 1
+        links[x + 3][y].bridge[0] += 1
+         
 
 
 @jit
